157acf740bb27f50ed589c823902d5e8520c7e9e
Bill Newman <william.newman@seattlecolleges.edu>
2022-08-09 18:09:28 +0000

    Adds routes to our index.js file

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-var http = require("http");
+var http = require("http"); 
 http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
+  var path = req.url.toLowerCase();
+  switch(path) {
+    case '/':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Home page');
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('About page');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not found');
+      break;
+    }
 }).listen(process.env.PORT || 3000);
+
